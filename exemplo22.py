035c37d3c4782eccca5c3fe6e66d2742224ae8cc
Bianca Pereira da Silva <134341356+biancapereirasilva@users.noreply.github.com>
2026-03-12 14:53:40 -0300

    Add files via upload

--- exemplo22.py
+++ exemplo22.py
@@ -1,7 +1,11 @@
-
-
-
-
-
-
 valor = input("Base do triângulo: ")
+base = float(valor)
+
+altura = float(input("Altura do triângulo: "))
+
+area = base * altura / 2
+
+print("A área vale", area)
+
+area2 = round(area, 2) #arredondando para 2 casas decimais
+print("A área vale " + str(area2))
